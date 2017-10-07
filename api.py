97291076237c583e16525d67554026820a97b308
Ap0c <Ap0c@users.noreply.github.com>
2017-10-07 20:56:40 +0100

    Added endpoint to return hue lights as JSON.

--- api.py
+++ api.py
@@ -1,8 +1,17 @@
 # ----- Imports ----- #
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
+import hue
 
 
 # ----- Setup ----- #
 
 api = Blueprint('api', __name__)
+
+
+# ----- Functions ----- #
+
+@api.route('/lights', methods=['GET'])
+def lights():
+
+    return jsonify({ "lights": hue.scan() })
